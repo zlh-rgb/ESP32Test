e24ad3bcad8689026ca4ea35bb9d842730a4e5a3
zlh-rgb <1367825047@qq.com>
2020-10-10 03:17:27 +0800

    master

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,19 @@
 #define TFT_DC 12
 #define TFT_RST 14
 
+char buffer[20];
+
 BNO080 myIMU;
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC,TFT_RST);
 
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("BNO080 Read Example");
+  // Serial.begin(115200);
+  // Serial.println();
+  // Serial.println("BNO080 Read Example");
   tft.begin();
+  tft.setRotation(2);
   tft.fillScreen(ILI9341_BLUE);
 
   Wire.begin();
@@ -25,19 +28,21 @@ void setup() {
   Wire.setClock(400000); //Increase I2C data rate to 400kHz
 
   myIMU.enableStepCounter(500); //Send data update every 500ms
-
-  Serial.println(F("Step Counter enabled"));
-  Serial.println(F("Step count since sketch started:"));
+  myIMU.enableGyro(50);
 }
 
 void loop() {
     if (myIMU.dataAvailable() == true)
   {
     unsigned int steps = myIMU.getStepCount();
-
-    Serial.print(steps);
-    Serial.print(F(","));
-
-    Serial.println();
+    float X = myIMU.getGyroX();
+    
+    tft.setCursor(0, 0);
+    tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(1);
+    sprintf(buffer,"steps:%d",steps);
+    tft.println(buffer);
+    sprintf(buffer,"X:%.2f",X);
+    tft.println(buffer);
   }
+  
 }

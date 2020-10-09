1c7204a2b6690d8f27a069aa8c52c95c79aa1d4c
ActivePeter <1020401660@qq.com>
2020-10-10 03:19:58 +0800

    sss

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "bno080/SparkFun_BNO080_Arduino_Library.h" // Click here to get the library: http://librarymanager/All#SparkFun_BNO080
 #include <Adafruit_ILI9341.h>
+#include "Ads_112c04/Ads_112c04.h"
 
 #define TFT_CS 13
 #define TFT_DC 12
@@ -8,10 +9,12 @@
 
 BNO080 myIMU;
 
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC,TFT_RST);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
+Ads_112c04 &ads;
 
-
-void setup() {
+void setup()
+{
+  ads = Ads_112c04::instance;
   Serial.begin(115200);
   Serial.println();
   Serial.println("BNO080 Read Example");
@@ -22,6 +25,11 @@ void setup() {
 
   myIMU.begin();
 
+  ads.init(Ads_112c04::AxState::DGND, Ads_112c04::AxState::DGND);
+  ads.configRegister0(Ads_112c04::Gain::GAIN_1);
+  ads.configRegister1(Ads_112c04::SpeedOfSample::SPS_1000,Ads_112c04::Mode::Mode_Normal,Ads_112c04::ConvMode);
+  ads.startConv();
+
   Wire.setClock(400000); //Increase I2C data rate to 400kHz
 
   myIMU.enableStepCounter(500); //Send data update every 500ms
@@ -30,8 +38,13 @@ void setup() {
   Serial.println(F("Step count since sketch started:"));
 }
 
-void loop() {
-    if (myIMU.dataAvailable() == true)
+void loop()
+{
+  Serial.println("hhh");
+  double adc=ads.readADC();
+  Serial.println("adc:"+adc);
+  
+  if (myIMU.dataAvailable() == true)
   {
     unsigned int steps = myIMU.getStepCount();
 
